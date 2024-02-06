d8f28909196572a055a6c9fc3724cf2c19bd9d95
Gleb Klyga <klyga90@bk.ru>
2024-02-06 16:37:30 +0300

    Initial Commit

---
+++ MyLovesPlacesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLovesPlacesApp
+//
+//  Created by Глеб Клыга on 6.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
